c9c21def143a1604686eeda21193bb57d2c5cdab
proyectosbeta <josego85@gmail.com>
2020-06-20 16:33:08 -0400

    Ejemplo de OpenWeather

---
+++ public/assets/js/Constants.js
@@ -0,0 +1,10 @@
+const API_KEY_OPENWEATHER = 'c68ce3c6ed0552e3acf2749a6a2cd5b0';
+
+// Asuncion - Paraguay
+const DEFAULT_LNG = -57.62380;
+const DEFAULT_LAT = -25.299114;
+
+// Options Map.
+const DEFAULT_ZOOM = 8;
+const DEFAULT_ZOOM_MIN = 5;
+const DEFAULT_ZOOM_MAX = 8;
---
+++ public/assets/js/MapOSM.js
@@ -0,0 +1,29 @@
+function loadMap()
+{
+	let map;
+	let osm_layer;
+	let cities_layer;
+	
+	osm_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	{
+		minZoom: DEFAULT_ZOOM_MIN,
+		maxZoom: DEFAULT_ZOOM_MAX,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+	});
+
+	cities_layer = L.OWM.current(
+	{
+		intervall: 15,
+		imageLoadingUrl: 'assets/img/owmloading.gif',
+		lang: 'es',
+		minZoom: 5,
+		appId: API_KEY_OPENWEATHER
+	});
+
+	map = L.map('map',
+	{
+		center: new L.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+		zoom: DEFAULT_ZOOM,
+		layers: [osm_layer, cities_layer]
+	});
+};
