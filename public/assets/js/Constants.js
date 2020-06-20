f66b0061c9586a18cec9c0eb3a88086096bf3c9b
proyectosbeta <josego85@gmail.com>
2020-06-20 19:28:05 -0400

    Mejoras en el código.

--- public/assets/js/Constants.js
+++ public/assets/js/Constants.js
@@ -1,10 +1,10 @@
 const API_KEY_OPENWEATHER = 'c68ce3c6ed0552e3acf2749a6a2cd5b0';
 
 // Asuncion - Paraguay
-const DEFAULT_LNG = -57.62380;
 const DEFAULT_LAT = -25.299114;
+const DEFAULT_LNG = -57.62380;
 
 // Options Map.
-const DEFAULT_ZOOM = 8;
-const DEFAULT_ZOOM_MIN = 5;
-const DEFAULT_ZOOM_MAX = 8;
+const DEFAULT_ZOOM = 9;
+const DEFAULT_MIN_ZOOM = 5;
+const DEFAULT_MAX_ZOOM = 9;
--- public/assets/js/MapOSM.js
+++ public/assets/js/MapOSM.js
@@ -1,16 +1,15 @@
-function loadMap()
+function loadMap ()
 {
 	let map;
 	let osm_layer;
 	let cities_layer;
-	
-	osm_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-	{
-		minZoom: DEFAULT_ZOOM_MIN,
-		maxZoom: DEFAULT_ZOOM_MAX,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-	});
 
+	osm_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		minZoom: DEFAULT_MIN_ZOOM,
+		maxZoom: DEFAULT_MAX_ZOOM,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
+	});
+	
 	cities_layer = L.OWM.current(
 	{
 		intervall: 15,
@@ -20,10 +19,9 @@ function loadMap()
 		appId: API_KEY_OPENWEATHER
 	});
 
-	map = L.map('map',
-	{
+	map = L.map('map', {
 		center: new L.LatLng(DEFAULT_LAT, DEFAULT_LNG),
 		zoom: DEFAULT_ZOOM,
 		layers: [osm_layer, cities_layer]
 	});
-};
+}
